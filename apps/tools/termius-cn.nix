de98617271a5d1c3c57349fb68ec3bad68daa007
davyjones <cordeliahoward@outlook.com>
2026-05-10 12:30:56 +0800

    remove dwm and add minmium fix

--- apps/tools/termius-cn.nix
+++ apps/tools/termius-cn.nix
@@ -2,7 +2,7 @@
   # 定义汉化包
   termius-zh-asar = pkgs.fetchurl {
     url = "https://github.com/ArcSurge/Termius-Pro-zh_CN/releases/download/v9.38.1/app-linux-localize.asar";
-    hash = "sha256-ALezMEzSBP/IQPdV15gqXgyZVJ4sOCaGdm8sVfFBZ6E=";
+    hash = "sha256-W+poHpea24lXsoZ9NxEd4ycnX0rpqRAiZ7+8K4Ck5Yg=";
   };
 
   # 包装原有的 Termius
