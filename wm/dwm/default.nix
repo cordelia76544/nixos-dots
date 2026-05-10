fa2550a018a1f34fdc82e5068c0214bdba6c0289
davyjones <cordeliahoward@outlook.com>
2026-05-10 10:29:56 +0800

    improve dwm

--- wm/dwm/default.nix
+++ wm/dwm/default.nix
@@ -3,7 +3,7 @@
     ./picom.nix
     #./dwm.nix # enabled in configuration.nix
     ./rofi.nix
-    ./autostart.sh
+    ./autostart.nix
   ];
 
   home.sessionVariables = {
--- wm/dwm/dwm.nix
+++ wm/dwm/dwm.nix
@@ -18,15 +18,4 @@
       src = ./slstatus;
     }))
   ];
-
-  home.sessionVariables = {
-    GTK_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
-    XMODIFIERS = "@im=fcitx";
-
-    GDK_BACKEND = "x11";
-    QT_QPA_PLATFORM = "xcb";
-    SDL_VIDEODRIVER = "x11";
-    NIXOS_OZONE_WL = "0";
-  };
 }
