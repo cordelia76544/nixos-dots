629a19376d30c1d8c8174bff9aeb0c46c8b94245
davyjones <cordeliahoward@outlook.com>
2026-06-14 11:18:38 +0800

    fix dwm4

--- wm/polybar/default.nix
+++ wm/polybar/default.nix
@@ -3,4 +3,12 @@
     ./polybar.nix
     ./fonts
   ];
+
+  home.file.".config/polybar/launch.sh" = {
+    executable = true;
+    text = ''
+      #!/usr/bin/env bash
+      systemctl --user restart polybar.service &
+    '';
+  };
 }
