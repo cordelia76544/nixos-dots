45e53f19ef792e60f7ccb659a4eeac2d4d303fc7
davyjones <cordeliahoward@outlook.com>
2026-06-19 17:13:01 +0800

    change wechat from flatpak to nixpkgs, and try to fix nushell his

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
     daeuniverse.url = "github:daeuniverse/flake.nix";
     stylix.url = "github:nix-community/stylix/release-26.05";
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.7.0";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,11 +23,6 @@
       url = "github:cordelia76544/helium-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    #firefox-addons = {
-    #  url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #r};
   };
 
   outputs = {
@@ -41,7 +35,6 @@
     nixvim,
     helium,
     stylix,
-    nix-flatpak,
     ...
   } @ inputs: {
     nixosConfigurations = {
@@ -61,7 +54,6 @@
           stylix.nixosModules.stylix
           inputs.daeuniverse.nixosModules.dae
           inputs.daeuniverse.nixosModules.daed
-          nix-flatpak.nixosModules.nix-flatpak
           (
             {pkgs, ...}: {
               nixpkgs.overlays = [nix-cachyos-kernel.overlays.pinned];
