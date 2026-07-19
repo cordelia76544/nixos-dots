ba965c4c013091eb551ecb929a8ce1eb0bba33a4
davyjones <cordeliahoward@outlook.com>
2026-07-19 11:05:05 +0800

    fix

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
     zedg.url = "github:cordelia76544/zedg-nix";
     impermanence.url = "github:nix-community/impermanence";
-    inputs.nixvim.url = "github:nix-community/nixvim-26.05";
+    nixvim.url = "github:nix-community/nixvim-26.05";
   };
 
   outputs =
